af7f796c41c37d191b85750b4287e3e5cb8db8d6
Thomas-474 <teg2004@icloud.com>
2021-09-27 10:42:32 -0400

    classes & txt file

---
+++ Nerd Labyrinth/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerd_Labyrinth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.ReadKey();
+        }
+    }
+
+    public class Player
+    {
+        public string name;
+        public int age;
+        public double luck;
+        public int points;
+        public bool hasKey;
+    }
+
+    public class Room
+    {
+        public int number;
+        public string type; // Exit Room or Type of Puzzle
+        public int lengthPosition; // x-axis
+        public int heightPosition; // y-axis
+        public int widthPosition; // z-axis
+    }
+}
