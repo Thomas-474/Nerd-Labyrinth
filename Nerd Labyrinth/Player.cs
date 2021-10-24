2895b874d53d8b23267d81abbc42514fe8d8b5ee
Thomas-474 <70166771+Thomas-474@users.noreply.github.com>
2021-10-24 09:41:06 -0400

    Part 1 Done

--- Nerd Labyrinth/Player.cs
+++ Nerd Labyrinth/Player.cs
@@ -1,22 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nerd_Labyrinth
+class Player
 {
-    class Player
-    {
-        public string Name
-        { get; set; }
-        public int Age
-        { get; set; }
-        public double Luck
-        { get; set; }
-        public double Points
-        { get; set; }
-        public bool HasKey
-        { get; set; }
-    }
+  public string Number;
+	public string Name;
+  public int Age;
+  public double Luck = LuckGenerator();
+  public double Points;
+	public int D10 = Roll();
+
+	public Player(string number, string name, int age, double points)
+	{
+		this.Number = number;
+		this.Name = name;
+		this.Age = age;
+		this.Points = points;
+	}
+
+	public static double LuckGenerator()
+  {
+		Random random = new Random();
+    double luck = random.NextDouble() + 1;
+    return luck;
+  }
+
+	public static int Roll()
+	{
+		Random random = new Random();
+		int d10 = random.Next(1,10);
+		return d10;
+	}
+
 }
