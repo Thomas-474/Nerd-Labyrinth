25658de78ce708eb0f91fcf49e9761bc38a06442
Thomas-474 <70166771+Thomas-474@users.noreply.github.com>
2021-10-24 09:03:48 -0400

    Part 1 Done

--- Nerd Labyrinth/Program.cs
+++ Nerd Labyrinth/Program.cs
@@ -8,21 +8,13 @@ namespace Nerd_Labyrinth
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            /*
-            Room room1 = new Room(numberGuesser);
-            Room room2 = new Room(numberGuesser);
-            Room room3 = new Room(numberGuesser);
-            Room room4 = new Room(numberGuesser);
-            Room room5 = new Room(numberGuesser);
-            Room room6 = new Room(numberGuesser);
-            Room room7 = new Room(numberGuesser);
-            Room room8 = new Room(numberGuesser);
-            Room room9 = new Room(numberGuesser);
-            */
+        public static void Main (string[] args)
+	    {
+		GameSetup.CreatePlayers();
+		GameSetup.DiceTest();
 
-            Console.ReadKey();
+		
+        Console.ReadKey();
         }
     }
 }
